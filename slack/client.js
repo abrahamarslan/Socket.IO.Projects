e194d0a2b714483889147345f6f1c0dfd8a1a6a7
abrahamarslan <arslan@abrahamarslan.com>
2020-09-27 19:46:13 +0530

    Data

--- slack/client.js
+++ slack/client.js
@@ -1,4 +1,5 @@
 const socket = io('http://localhost:9000');
+
 console.log(socket);
 //Send message when the client establishes the connection with the server
 socket.on('connect', (data) => {
--- slack/server.js
+++ slack/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const socketio = require('socket.io');
-
 app.use(express.static(__dirname + '/public'));
-
+let namespaces = require('./data/namespace.js');
+console.log(namespaces);
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
