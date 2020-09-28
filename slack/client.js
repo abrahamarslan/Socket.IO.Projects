fedb2817bb68a31460bd29946af51018fa1df33c
abrahamarslan <arslan@abrahamarslan.com>
2020-09-28 12:37:17 +0530

    Slack - Client

--- slack/client.js
+++ slack/client.js
@@ -1,7 +1,24 @@
 const socket = io('http://localhost:9000');
-
-console.log(socket);
 //Send message when the client establishes the connection with the server
 socket.on('connect', (data) => {
+    socket.on('nsList', (namespaces) => {
+       let namespacesDiv = document.querySelector('.namespaces');
+       namespacesDiv.innerHTML = "";
+       //Add the namespace list dynamically
+       namespaces.forEach((namespace) => {
+           namespacesDiv.innerHTML += `<div class="namespace" ns="${namespace.endpoint}"><img src="${namespace.img}"></div>`;
+       });
 
+    //Wait for above to lead
+    var nameSpaceCollction = document.getElementsByClassName("namespace");
+    var nameSpaceArray = Array.from(nameSpaceCollction); 
+    console.log(nameSpaceArray); 
+    (Array.from(nameSpaceCollction)).forEach((elm) => { 
+      elm.addEventListener('click', (event) => { 
+        console.log('clicked'); 
+        const endpoint = elm.getAttribute('ns'); 
+        console.log(endpoint); 
+      }); 
+    });
+  });
 });
--- slack/server.js
+++ slack/server.js
@@ -3,10 +3,18 @@ const app = express();
 const socketio = require('socket.io');
 app.use(express.static(__dirname + '/public'));
 let namespaces = require('./data/namespace.js');
-console.log(namespaces);
+//console.log(namespaces);
 const expressServer = app.listen(9000);
 const io = socketio(expressServer);
 
 io.on('connection', (socket) => {
-
+    //build an array to send back with the image and endpoint for each Namespace
+    let namespaceData = namespaces.map((namespace) => {
+        return {
+            img: namespace.img,
+            endpoint: namespace.endpoint
+        }
+    });
+    //Send this list to the clients
+    socket.emit('nsList', namespaceData);
 });
