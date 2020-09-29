aa55006ef6b3a3e96695163211849cd713ac7c57
abrahamarslan <arslan@abrahamarslan.com>
2020-09-29 19:34:31 +0530

    Initial - Agar.io

---
+++ Agar.IO Clone/public/ui.js
@@ -0,0 +1,28 @@
+let wHeight = $(window).height();
+let wWidth = $(window).width();
+let player = {}; // Player object
+
+let canvas = document.querySelector('#the-canvas');
+let context = canvas.getContext('2d');
+canvas.width = wWidth;
+canvas.height = wHeight;
+
+// Load the login modal
+$(window).load(function(){
+    $('#loginModal').modal('show');
+});
+
+$('.name-form').submit((event) => {
+    event.preventDefault();
+    player.name = $('#name-input').val();
+    console.log(player);
+    $('#loginModal').modal('hide');
+    $('#spawnModal').modal('show');
+    $('.player-name').html(player.name);
+});
+
+$('.start-game').click((event) => {
+    // Hide all modals
+    $('.modal').modal('hide');
+    $('.hiddenOnStart').removeAttr('hidden');
+});
